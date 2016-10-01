cfe21c37136d0e64078bf4ed9e9b4d5bee329b9b
Guillaume Legrain <legrainguillaume@gmail.com>
2016-10-01 18:11:56 +0200

    refactored ViewController to take advantage of swift features

--- Altimeter/ViewController.swift
+++ Altimeter/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import CoreLocation
 
 class ViewController: UIViewController, CLLocationManagerDelegate {
-    
+
     @IBOutlet weak var altitudeLabel: UILabel!
     @IBOutlet weak var verticalAccuracyLabel: UILabel!
     @IBOutlet weak var horizontalAccuracyLabel: UILabel!
@@ -19,114 +19,165 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     @IBOutlet weak var horizontalAccuracyProgress: SignalStrengthView!
     @IBOutlet weak var shareButton: UIBarButtonItem!
 
-    private var location: CLLocation?
     private let locationManager = CLLocationManager()
     private let altitudeFormatter = AltitudeFormatter()
 
+    private var location: CLLocation? {
+        didSet {
+            updateUI()
+        }
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         // Request the appropriate type of authorization from the user.
         locationManager.requestWhenInUseAuthorization()
-        
+
         // Assign Core Location Manager delegation
         locationManager.delegate = self
 
         // NOTE: location manager will start updating location in the locationManager:didChangeAuthorizationStatus delegate method
 
     }
-    
-    // MARK: - Core Location Delegate
+
+    // MARK: - Core Location Manager Delegate
 
     func locationManager(manager: CLLocationManager, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
         // Configure location service
         locationManager.desiredAccuracy = kCLLocationAccuracyBestForNavigation
         if status == .AuthorizedAlways || status == .AuthorizedWhenInUse {
             locationManager.startUpdatingLocation()
-            //locationManager.allowsBackgroundLocationUpdates = true
+            // locationManager.allowsBackgroundLocationUpdates = true
+        } else {
+            locationManager.stopUpdatingLocation()
         }
     }
 
     func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         if let location = locations.last {
             self.location = location
-
-            // Format altitude
-            let altitudeString = altitudeFormatter.mutableAttributtedStringFromLocationDistance(location.altitude)
-            
-            // Set accuracy labels
-            // NOTE: negative value in accuracy indicates that the altitude value is invalid.
-            //       Accuracy is in meters.
-            var verticalAccuracyString = ""
-            if location.verticalAccuracy < 0 {
-                verticalAccuracyString = "Not available"
-            } else {
-                verticalAccuracyString = "± \(lround(location.verticalAccuracy)) m"
-            }
-            #if (TARGET_IPHONE_SIMULATOR)
-                verticalAccuracyString = "± 5 m"
-            #endif
-            
-            // Update UI
-            verticalAccuracyLabel.text! = "Vertical accuracy: \(verticalAccuracyString)"
-            horizontalAccuracyLabel.text! = "Horizontal accuracy: ± \(lround(location.horizontalAccuracy)) m"
-            verticalAccuracyProgress.setProgress(Float(location.verticalAccuracy), animated: true)
-            horizontalAccuracyProgress.setProgress(Float(location.horizontalAccuracy), animated: true)
-            
-            #if (TARGET_IPHONE_SIMULATOR)
-                // Set Progress bars and altitude string for debug
-                updateProgressBar(verticalAccuracyProgress, withAccuracy: 1000)
-                updateProgressBar(horizontalAccuracyProgress, withAccuracy: 1000)
-                altitudeString = altitudeFormatter.mutableAttributtedStringFromLocationDistance(0)
-            #endif
-            
-            altitudeLabel.attributedText = altitudeString
-
-            coordsLabel.text = CoordinateFormatter().stringFromLocationCoordinate(location.coordinate)
         }
+
+        // Set fake altitude and location for simulator
+        #if (arch(i386) || arch(x86_64)) && os(iOS)
+            let fakeLocation = CLLocation(
+                coordinate: CLLocationCoordinate2D(latitude: 45.934613, longitude: 6.970240),
+                altitude: 4122.3,
+                horizontalAccuracy: 20,
+                verticalAccuracy: 5,
+                timestamp: NSDate(timeIntervalSince1970: 1462782883)
+            )
+            self.location = fakeLocation
+        #endif
     }
-    
+
     func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
-//        print("Error description: \(error.localizedDescription).")
-//        print("Error suggestion: \(error.localizedRecoverySuggestion).")
-//        print("Error failure reason: \(error.localizedFailureReason).")
-//        print("Error recovery options: \(error.localizedRecoveryOptions).")
-        
+
         var alert: UIAlertController
         let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel, handler: nil)
         let openAction = UIAlertAction(title: "Open Settings", style: .Default) { (action) in
-            if let url = NSURL(string:UIApplicationOpenSettingsURLString) {
+            if let url = NSURL(string: UIApplicationOpenSettingsURLString) {
                 UIApplication.sharedApplication().openURL(url)
             }
         }
-        
+
         switch CLLocationManager.authorizationStatus() {
         case .Denied, .Restricted:
             alert = UIAlertController(
                 title: "Location Access Disabled",
-                message: "In order to get your altitude, please open this app's settings and set location access to 'Always'.",
+                message: "In order to get your altitude, please open this app's settings and set location access to 'Always' or 'While Using the App.",
                 preferredStyle: .Alert
             )
             alert.addAction(cancelAction)
             alert.addAction(openAction)
+            self.locationManager.stopUpdatingLocation()
         default:
-            alert = UIAlertController(title: "Error", message: error.localizedDescription, preferredStyle: .Alert)
+            alert = UIAlertController(title: "Error", message:"Your location could not be retreived.", preferredStyle: .Alert)
             alert.addAction(cancelAction)
+            // The location manager does not stop updating location because the signal might come back
         }
         presentViewController(alert, animated: true, completion: nil)
 
-        // Replace altitude label with a "No data" text
-        let attributes = [
-            NSFontAttributeName: UIFont.systemFontOfSize(17),
-            NSForegroundColorAttributeName: UIColor.darkGrayColor()
-        ]
-        altitudeLabel.attributedText = NSAttributedString(string: "No data", attributes: attributes)
-        // Disable share button
-        shareButton.enabled = false
+        self.location = nil
 
     }
 
     // MARK: - UI
+
+    private func updateUI() {
+
+        // Disable share button if location is unavailable
+        shareButton.enabled = (location != nil)
+
+        altitudeLabel.attributedText = altitudeDescription
+
+        coordsLabel.text = coordinatesDescription
+
+        verticalAccuracyLabel.text! = verticalAccuracyDescription
+        horizontalAccuracyLabel.text! = horizontalAccuracyDescription
+
+        if location != nil {
+            verticalAccuracyProgress.setProgress(Float(location!.verticalAccuracy), animated: true)
+            horizontalAccuracyProgress.setProgress(Float(location!.horizontalAccuracy), animated: true)
+        } else {
+            verticalAccuracyProgress.setProgress(0, animated: true)
+            horizontalAccuracyProgress.setProgress(0, animated: true)
+        }
+
+    }
+
+    private var altitudeDescription: NSAttributedString {
+        // Create formatted altitude string
+        if let altitude = location?.altitude {
+             return altitudeFormatter.mutableAttributtedStringFromLocationDistance(altitude)
+        } else {
+            // Replace altitude label with a "No data" text
+            let attributes = [
+                NSFontAttributeName: UIFont.systemFontOfSize(17),
+                NSForegroundColorAttributeName: UIColor.darkGrayColor()
+            ]
+            return NSAttributedString(string: "No data", attributes: attributes)
+        }
+    }
+
+    // Create accuracy descriptions
+    // NOTE: negative value in accuracy indicates that the altitude value is invalid.
+    //       Accuracy is in meters.
+    private var verticalAccuracyDescription: String {
+        if let verticalAccuracy = location?.verticalAccuracy where verticalAccuracy > 0 {
+            var description = "Vertical accuracy: "
+            if altitudeFormatter.unit == .Feet {
+                description += "± \(lround(verticalAccuracy * 3.28084)) ft"
+            } else  {
+                description += "± \(lround(verticalAccuracy)) m"
+            }
+            return description
+        } else {
+             return "Altitude invalid"
+        }
+    }
+
+    private var horizontalAccuracyDescription: String {
+        if let horizontalAccuracy = location?.horizontalAccuracy where horizontalAccuracy > 0 {
+            var description = "Horizontal accuracy: "
+            if altitudeFormatter.unit == .Feet {
+                description += "± \(lround(horizontalAccuracy * 3.28084)) ft"
+            } else {
+                description += "± \(lround(horizontalAccuracy)) m"
+            }
+            return description
+        } else {
+            return "Location invalid"
+        }
+    }
+
+    private var coordinatesDescription: String? {
+        if let coordinate = location?.coordinate {
+            return CoordinateFormatter().stringFromLocationCoordinate(coordinate)
+        }
+        return nil
+    }
 
     @IBAction func changeAltitudeUnit() {
         // TODO: save to user defaults
@@ -136,10 +187,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
             altitudeFormatter.unit = .Meters
         }
 
-        // Update altitude label (i.e. do not wait for the next location update)
-        if let altitude = location?.altitude {
-            altitudeLabel.attributedText = altitudeFormatter.mutableAttributtedStringFromLocationDistance(altitude)
-        }
+        updateUI()
 
     }
 
@@ -155,5 +203,5 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     override func preferredStatusBarStyle() -> UIStatusBarStyle {
         return UIStatusBarStyle.LightContent
     }
-    
+
 }
